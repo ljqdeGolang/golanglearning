c68129389227dcf4c723fe99a10526c9d502c7b8
ljqdegittree <18896659405@163.com>
2021-01-11 08:59:30 +0800

    commit

--- golang/test.go
+++ golang/test.go
@@ -1,27 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-type Animal interface {
-	eat()
-}
-
-type Cat struct {
-	name string
-}
-
-func (cat Cat) eat() {
-	fmt.Println(cat.name + "猫吃东西")
-}
-
-type Dog struct{}
-
-func (dog Dog) eat() {
-	fmt.Println("狗吃东西")
-}
 func main() {
-	var animal1 Animal = Cat{"maomao"}
-	var animal2 Animal = Dog{}
-	animal1.eat()
-	animal2.eat()
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
 }
